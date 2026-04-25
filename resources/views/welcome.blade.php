f1c76dbabe8eaa77c8edab54422aae782894377c
KokoFibo <kokonacci@gmail.com>
2026-04-25 12:31:41 +0700

    rubah versi 1.2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -863,7 +863,7 @@
         @endif
 
         <div class="text-2xl text-center lg:text-5xl ">
-            Kokofibo Invoicing System v 1.1
+            Kokofibo Invoicing System v 1.2
         </div>
     </div>
 </body>
